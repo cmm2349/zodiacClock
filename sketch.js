db0a40cc72003c507006c4186742a244cec57f8a
cmm2349 <75586824+cmm2349@users.noreply.github.com>
2021-01-15 16:54:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -163,7 +163,7 @@ function draw() {
       if(i == 10){scorpio.mask(quadmask);
                image(scorpio, -110, -245, 80, 75);}
       if(i == 11){sagittarius.mask(quadmask);
-               image(sagittarius, -110, -255, 100, 90);}     
+               image(sagittarius, -110, -255, 80, 90);}     
    
   translate(p5.Vector.fromAngle(0.5236*(i), 117)); //translate every symbol so they form a circle
 }
